5f2f18021ca8d5099cb05ec4c60faec1020bf182
jeongsoo <rpsha33@naver.com>
2019-06-24 19:16:12 +0900

    enzyme + jest

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
-import Profile from "./Profile";
 
-const App = () => <Profile id={'good'} name={'kim'} />
+const App = () => <div> good</div>
 
 export default hot(module)(App)
