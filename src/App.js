29898ed5b4dddba50e29070fc43d156e60898548
jeongsso <rpsha33@naver.com>
2019-09-09 23:50:22 +0900

    hot-loader 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { hot } from 'react-hot-loader'
 
 const App = () => <div>Good</div>
 
-export default hot(module)(App)
+export default App
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,9 +2,11 @@ const Merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
 
 module.exports = Merge(baseConfig, {
-   mode: 'development',
-   devtool: 'inline-source-map',
-   devServer: {
-      port: 9999,
-   }
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 9999,
+    historyApiFallback: true,
+    hot: true
+  }
 })
