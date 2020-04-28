87868b07cce5f96b4c0468509891454a1e859b53
jeongsoo <rpsha33@naver.com>
2020-04-28 12:33:39 +0900

    tdd

--- test/Counter.test.js
+++ test/Counter.test.js
@@ -3,11 +3,19 @@ import { shallow, mount, render  } from 'enzyme'
 import HookCounter from '../src/component/HookCounter'
 import Counter from '../src/component/Counter'
 
+const arrayInMaxValue = (arr) => Math.max(...arr)
+const arrayInAverageValue = (arr) => 
+   arr.reduce((acc, current, { length }) =>
+      acc + current / length
+   , 0)
+
+
+
 describe('카운터 테스트', () => {
-   it('스냅샷 매치', () => {
-      const wrapper = shallow(<HookCounter />)
-      expect(wrapper).toMatchSnapshot();
-   });
+   // it('스냅샷 매치', () => {
+   //    const wrapper = shallow(<HookCounter />)
+   //    expect(wrapper).toMatchSnapshot();
+   // });
    // it('초기 값', () =>  {
    //    const wrapper = shallow(<Counter />)
    //    expect(wrapper.find('h2').text()).toBe('0')
@@ -31,11 +39,17 @@ describe('카운터 테스트', () => {
    //    plusButton.simulate('click');
    //    expect(wrapper.state().number).toBe(1)
    // })
-   it('감소 버튼 클릭 시뮬', () => {
-      const wrapper = mount(<HookCounter />)
-      wrapper.find('#mb').simulate('click')
-      const number = wrapper.find('h2').text()
-      expect(number).toBe("-1")
+   // it('감소 버튼 클릭 시뮬', () => {
+   //    const wrapper = mount(<HookCounter />)
+   //    wrapper.find('#mb').simulate('click')
+   //    const number = wrapper.find('h2').text()
+   //    expect(number).toBe("-1")
+   // })
+   it('MaxValue In array', ()  => {
+      expect(arrayInMaxValue([1,2,3,4]) / 2).toBe(2)
+   })
+   it('Average In array', ()  => {
+      expect(arrayInAverageValue([70,80])).toBe(75)
    })
 
 
