a53e5cfcfca48bd50bc4e1c2daadc26efbde2748
jeongsso <rpsha33@naver.com>
2021-05-18 18:57:35 +0900

    fetch module add

--- src/routes/__tests__/routes.test.js
+++ src/routes/__tests__/routes.test.js
@@ -3,23 +3,25 @@ import { render, fireEvent, waitFor } from '@testing-library/react';
 
 import App from '@src/App';
 
-describe('Routes Test', () => {
+describe.skip('Routes Test', () => {
   let mountedComponent;
 
   beforeEach(() => {
     mountedComponent = render(<App />);
   });
 
-  it.skip('input link click', async () => {
+  it('input link click', async () => {
     const { getByTestId, queryByTestId } = mountedComponent;
 
     await waitFor(() => {
       const value = getByTestId('nav-input');
-      fireEvent.click(value);
-      const { pathname } = location;
-      expect(pathname).toBe('/input');
 
+      fireEvent.click(value);
+
+      const { pathname } = location;
       const input = queryByTestId('input');
+
+      expect(pathname).toBe('/input');
       expect(input).toBeInTheDocument();
     });
   });
